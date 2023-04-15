33832a5c9d74579729f1e61e129b727bbb1caf04
Inès Derraz <i.derraz@my-goodlife.fr>
2023-04-15 15:15:55 +0200

    display order number

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,3 +1,6 @@
 const urlParams = new URLSearchParams(window.location.search);
 const orderId = urlParams.get('orderId');
 
+orderNumber = document.querySelector("#orderId");
+console.log(orderId)
+orderNumber.innerText = orderId
